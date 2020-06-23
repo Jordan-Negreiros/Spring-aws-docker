7c097f9e8f1c2644482268ddbc917dd42009b3b3
Jordan <jordannegreirossantos@gmail.com>
2020-06-23 12:25:04 -0300

    chave assimetrica para acessar as instancias

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,12 +1,22 @@
+resource "aws_key_pair" "keypair" {
+  public_key = file("key/beerstore_key.pub")
+}
+
 resource "aws_instance" "instances" {
   count = 3
 
   ami = "ami-09d95fab7fff3776c"
   instance_type = "t2.micro"
 
-  subnet_id = "${element(aws_subnet.public_subnet.*.id, count.index)}"
+  subnet_id = element(aws_subnet.public_subnet.*.id, count.index)
+
+  key_name = aws_key_pair.keypair.key_name
 
   tags = {
     Name = "jordan_instances"
   }
+}
+
+output "public_ips" {
+  value = join(", ", aws_instance.instances.*.public_ip)
 }
