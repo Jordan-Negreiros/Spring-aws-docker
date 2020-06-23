936b1d53576ae269e67627fdf06f31ac0972bac2
Jordan <jordannegreirossantos@gmail.com>
2020-06-23 15:03:58 -0300

    provisionando Postgres com RDS

--- terraform/security.tf
+++ terraform/security.tf
@@ -9,3 +9,15 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["191.47.62.177/32"]
   }
 }
+
+resource "aws_security_group" "database" {
+  vpc_id = aws_vpc.main.id
+  name = "jordan_database"
+
+  ingress {
+    from_port = 5432
+    protocol = "tcp"
+    to_port = 5432
+    self = true
+  }
+}
